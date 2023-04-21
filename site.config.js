4beabf95dc45a0ac0aa472ea2d19c6cb9b60a393
Minghao-Liang <liangminghao0810@foxmail.com>
2023-04-21 22:56:35 +0800

    update

--- site.config.js
+++ site.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '009a15b0ef7eff0a96a836e6a719c3894643b940',
+  rootNotionPageId: '6c91451f5600493ca444bb2237104691',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '菜饼的技术博客',
-  domain: 'szufrank.top',
-  author: '菜饼同学',
+  name: 'Minghao Liang',
+  domain: 'Minghao-Liang.top',
+  author: 'Minghao Liang',
 
   // open graph metadata (optional)
-  description: '菜饼同学: 专注硬核技术，校招打法，个人认知体系成长',
-  socialImageTitle: '菜饼同学',
+  description: 'Minghao Liang\'s Blog',
+  socialImageTitle: 'Minghao Liang',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
   twitter: null,
-  github: 'frankcbliu',
-  wechatPublicName: '菜饼不菜',
-  wechatPublicURL: 'https://mp.weixin.qq.com/s?__biz=MzkxMzIyOTI1Mg==&mid=2247486027&idx=1&sn=2840f0b8cfdc982b9095dd18ec9892e5',
-  notionPublic: 'https://frankcbliu.notion.site/6a76fdd9ec6d4ae2855aa790d620eec4',
+  github: 'Minghao-Liang',
+  wechatPublicName: 'Minghao Liang',
+  wechatPublicURL: null,
+  notionPublic: 'https://www.notion.so/Minghao-Liang-6c91451f5600493ca444bb2237104691',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -34,7 +34,7 @@ module.exports = {
   imageCDNHost: null,
 
   // Utteranc.es comments via GitHub issue comments (optional)
-  utterancesGitHubRepo: 'frankcbliu/awesome-nextjs-notion-blog',
+  utterancesGitHubRepo: 'Minghao-liang/awesome-nextjs-notion-blog',
   utterancesGitHubLabel: '博客评论',
 
   // whether or not to enable support for LQIP preview images (optional)
